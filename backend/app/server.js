f9ae8d4f5777fda96dbbbfb839a3d281b3c6f4c7
Abbas <98712750+abiasV@users.noreply.github.com>
2024-06-16 22:14:21 -0400

    Update server.js

--- backend/app/server.js
+++ backend/app/server.js
@@ -37,7 +37,7 @@ class Application {
   }
   configServer() {
     this.#app.use(
-      cors({ credentials: true, origin: ["https://codelance-mern-front.vercel.app/"], methods: ["POST", "GET"] })
+      cors({ credentials: true, origin: ["https://codelance-mern-front.vercel.app"], methods: ["POST", "GET"] })
     );
     this.#app.use(express.json());
     this.#app.use(express.urlencoded({ extended: true }));
